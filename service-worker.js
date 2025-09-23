6d466554e212cbb879e5a3031e7b5ccc3ff7ee99
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2025-09-23 13:37:15 +0300

    ipad

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='onirim-v25';
+const CACHE='onirim-v26';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k===CACHE?null:caches.delete(k)))));self.clients.claim();});
